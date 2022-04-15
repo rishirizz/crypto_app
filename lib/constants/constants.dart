ec2ac438666db52db0d0750d0e8efeb7cb7f6199
Arijeet <arijeet@sbox.ltd>
2022-04-15 13:47:47 +0530

    Data rendered in the UI

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,3 @@
 const String apiKey = 'f7fee607b903c2a03fa8d12bc79e0ab2f5d16323';
 const String cryptoDomain =
-    "https://api.nomics.com/v1/currencies/ticker?key=$apiKey&per-page=100";
+    "https://api.nomics.com/v1/currencies/ticker?key=$apiKey&per-page=10";
--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:crypto_app/api_services/crypto_api.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DashBoardScreen extends StatefulWidget {
   const DashBoardScreen({Key? key}) : super(key: key);
@@ -9,18 +10,71 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class _DashBoardScreenState extends State<DashBoardScreen> {
+  List cryptoCoins = [];
   @override
   void initState() {
     super.initState();
-    getCryptoCurrency();
+    getMyCryptoCurrency();
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListView.builder(
+            itemCount: cryptoCoins.length,
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) => Card(
+              elevation: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SvgPicture.network(
+                        cryptoCoins[index]['logo_url'],
+                        height: 100,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            cryptoCoins[index]['name'],
+                          ),
+                          Text(
+                            'Price : \$ ${cryptoCoins[index]['price']}',
+                          ),
+                          Text(
+                            'Market Capital : \$ ${cryptoCoins[index]['market_cap']}',
+                          ),
+                          Text(
+                            'Rank : ${cryptoCoins[index]['rank']}',
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  Future getMyCryptoCurrency() async {
+    getCryptoCurrency().then((reponse) {
+      cryptoCoins = reponse;
+      debugPrint('CRYPTOCOINS=====> $cryptoCoins');
+    });
   }
 }
