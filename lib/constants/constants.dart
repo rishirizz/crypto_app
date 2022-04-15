b61b0921faf81d73be8de2125d57bcd44b0ca276
Arijeet <arijeet@sbox.ltd>
2022-04-15 13:20:59 +0530

    api key added and got the response

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,1 +1,3 @@
-const String cryptoDomain = 'https://api.coinmarketcap.com/v1/ticker/?limit=50';
+const String apiKey = 'f7fee607b903c2a03fa8d12bc79e0ab2f5d16323';
+const String cryptoDomain =
+    "https://api.nomics.com/v1/currencies/ticker?key=$apiKey&per-page=100";
