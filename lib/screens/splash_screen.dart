a33ba0fc30c183eb874a10cb85ccee51c43624ec
Arijeet <arijeet@sbox.ltd>
2022-04-16 11:02:36 +0530

    splash screen lottie added

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,7 +11,7 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  Duration delayInterval = const Duration(milliseconds: 2000);
+  Duration delayInterval = const Duration(milliseconds: 3000);
 
   @override
   void initState() {
@@ -19,22 +21,29 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
     return SafeArea(
       child: Scaffold(
         backgroundColor:
             (MediaQuery.of(context).platformBrightness == Brightness.light)
                 ? Colors.white
                 : const Color(0xff282828),
-        body: Center(
-          child: Text(
-            'CRYPTO TRACKER',
-            style: GoogleFonts.getFont(
-              'Architects Daughter',
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.teal,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('assets/crypto_graph.json'),
+            Text(
+              'CRYPTO TRACKER',
+              style: GoogleFonts.getFont(
+                'Architects Daughter',
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal,
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
